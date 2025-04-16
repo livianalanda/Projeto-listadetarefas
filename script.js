7a731a86fb4f38e0e0070fe79da06e05f7794d77
Lívia Nalanda <livianalanda16@gmail.com>
2025-04-16 14:48:05 -0300

    Adicionado riscar a tarefa + botão de excluir

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 function adicionarTarefa() {
     const input = document.getElementById("nova-tarefa");
     const texto = input.ariaValueMax.trim();
+
     if (texto === "") return;
+
     const li = document.createElement("li");
     li.textContent = texto;
+
+    // Clicar para riscar.
+    li.addEventListener("click", function () {
+        li.classList.toggle("riscada");
+    });
+
+    // Botão de excluir.
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.textContent = "X";
+    botaoExcluir.className = "excluir";
+    
+    botaoExcluir.addEventListener("click", function (event) {
+        event,stopPropagation();
+        li.remove();
+    });
+
+    li.appendChild(botaoExcluir);
+
     document.getElementById("lista-tarefas").appendChild(li);
     input.value = "";  
 }
